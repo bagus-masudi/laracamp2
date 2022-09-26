3434102a32523a7b91552bd9381136181a2e9a4a
Ahmad Bagus Mas'udi <ahmadbagusmasudi@gmail.com>
2022-09-26 17:40:38 +0700

    slicing success checkout

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,7 @@ Route::get('login', function () {
 Route::get('checkout', function () {
     return view('checkout/create');
 })->name('checkout');
+
+Route::get('checkout/success', function () {
+    return view('checkout/success');
+})->name('checkout.success');
