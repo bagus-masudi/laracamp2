33f157c7adec4e40af21ce5adc08a257e6e312ed
Ahmad Bagus Mas'udi <ahmadbagusmasudi@gmail.com>
2022-09-26 16:29:16 +0700

    campbenefit seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            CampTableSeeder::class
+            CampTableSeeder::class,
+            CampBenefitTableSeeder::class
         ]);
     }
 }
