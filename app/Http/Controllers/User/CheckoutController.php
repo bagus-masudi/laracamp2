65c8a6c9ce64a73aae378b0e7d50c98646bb076f
Ahmad Bagus Mas'udi <ahmadbagusmasudi@gmail.com>
2022-12-26 10:56:45 +0700

    update checkout

---
+++ app/Http/Controllers/User/CheckoutController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Models\Checkout;
+use App\Models\Camp;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CheckoutController extends Controller
+{
+    /**
+     * Display a listing of the resource. Hello
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Camp $camp)
+    {
+        return view('checkout.create', [
+            'camp' => $camp
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, Camp $camp)
+    {
+        $data = $request->all();
+        // MandMapping
+        $data['camp_id'] = $camp->id;
+        $data['user_id'] = Auth::id();
+
+        $user = Auth::user();
+        $user->name = $data['name'];
+        $user->email = $data['email'];
+        $user->occupation = $data['occupation'];
+        $user->save();
+
+        // Create Checkout
+        $checkout = Checkout::create($data);
+
+        return view('checkout/success');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function success(){
+        return view('checkout.success');
+    }
+}
